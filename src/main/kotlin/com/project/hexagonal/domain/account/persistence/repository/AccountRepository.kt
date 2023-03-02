02a831089d97d365c8c2ca609fa9c3e32096e668
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-02 22:34:52 +0900

    :sparkles: :: 영속성 계층 entity 추가

---
+++ src/main/kotlin/com/project/hexagonal/domain/account/persistence/repository/AccountRepository.kt
@@ -0,0 +1,12 @@
+package com.project.hexagonal.domain.account.persistence.repository
+
+import com.project.hexagonal.domain.account.persistence.entity.AccountEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+// 영속성이랑 도메인 계층을 구분하기 위해서 ORM Entity를 따로 만들었다.
+interface AccountRepository: JpaRepository<AccountEntity, Long>  {
+
+    fun existsByEmail(email: String): Boolean
+
+}
+
