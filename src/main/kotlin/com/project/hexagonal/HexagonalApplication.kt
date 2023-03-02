79e329ba2747ddea75aaa1e16bf3c22d2ac4d35a
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-02 12:26:55 +0900

    :tada: :: initial commit

---
+++ src/main/kotlin/com/project/hexagonal/HexagonalApplication.kt
@@ -0,0 +1,11 @@
+package com.project.hexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexagonalApplication
+
+fun main(args: Array<String>) {
+	runApplication<HexagonalApplication>(*args)
+}
