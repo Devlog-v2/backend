9b95c1f2d2b13fdb74045eea755cc3de064db6f5
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-03 16:14:15 +0900

    :sparkles: :: findAccountByEmail 메서드 추가

--- src/main/kotlin/com/project/hexagonal/domain/account/application/port/QueryAccountPort.kt
+++ src/main/kotlin/com/project/hexagonal/domain/account/application/port/QueryAccountPort.kt
@@ -1,7 +1,10 @@
 package com.project.hexagonal.domain.account.application.port
 
+import com.project.hexagonal.domain.account.Account
+
 interface QueryAccountPort {
 
     fun existsAccountByEmail(email: String)
+    fun findAccountByEmail(email: String): Account
 
 }
