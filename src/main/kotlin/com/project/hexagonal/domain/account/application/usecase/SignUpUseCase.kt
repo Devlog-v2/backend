e8a4bcb235864dca5871672775ed332dce736366
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-03 16:13:22 +0900

    :recycle: :: 요청 파라미터 도메인에서 dto로 변경

--- src/main/kotlin/com/project/hexagonal/domain/account/application/usecase/SignUpUseCase.kt
+++ src/main/kotlin/com/project/hexagonal/domain/account/application/usecase/SignUpUseCase.kt
@@ -1,16 +1,19 @@
 package com.project.hexagonal.domain.account.application.usecase
 
-import com.project.hexagonal.domain.account.Account
+import com.project.hexagonal.domain.account.adapter.web.data.request.SignUpRequest
+import com.project.hexagonal.domain.account.adapter.web.data.request.toDomain
 import com.project.hexagonal.domain.account.application.port.AccountPort
+import com.project.hexagonal.domain.account.application.port.PasswordEncodePort
 import com.project.hexagonal.global.annotation.UseCase
 
 @UseCase
 class SignUpUseCase(
-    private val accountPort: AccountPort
+    private val accountPort: AccountPort,
+    private val passwordEncodePort: PasswordEncodePort
 ) {
 
-    fun execute(domain: Account): Long =
-        accountPort.existsAccountByEmail(domain.email)
-            .let { accountPort.saveAccount(domain).idx }
+    fun execute(request: SignUpRequest): Long =
+        accountPort.existsAccountByEmail(request.email)
+            .let { accountPort.saveAccount(request.toDomain(), passwordEncodePort.encode(request.password)).idx }
 
 }
