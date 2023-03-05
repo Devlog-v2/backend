4a6851c2c11ea47a45ced704e9bc7abb7455ae16
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-05 23:06:01 +0900

    :recycle: :: 토큰 status 401에서 403으로 변경

--- src/main/kotlin/com/project/hexagonal/global/exception/error/ErrorCode.kt
+++ src/main/kotlin/com/project/hexagonal/global/exception/error/ErrorCode.kt
@@ -11,9 +11,9 @@ enum class ErrorCode(
     PASSWORD_NOT_CORRECT("비밀번호가 일치하지 않습니다.", 400),
 
     // JWT
-    INVALID_TOKEN_TPYE("유효하지 않은 토큰 타입 입니다.", 401),
-    INVALID_TOKEN("유효하지 않은 토큰 입니다.", 401),
-    EXPIRED_RERESH_TOKEN("만료된 refreshToken 입니다.", 401),
-    EXPIRED_ACCESS_TOKEN("만료된 accessToken 입니다.", 401),
+    INVALID_TOKEN_TPYE("유효하지 않은 토큰 타입 입니다.", 403),
+    INVALID_TOKEN("유효하지 않은 토큰 입니다.", 403),
+    EXPIRED_RERESH_TOKEN("만료된 refreshToken 입니다.", 403),
+    EXPIRED_ACCESS_TOKEN("만료된 accessToken 입니다.", 403),
 
 }
