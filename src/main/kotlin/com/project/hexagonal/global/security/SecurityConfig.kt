43342f69b2eea44a3ee67f49d11e0c04e6417829
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-05 23:09:29 +0900

    :sparkles: :: filter 등록 추가

--- src/main/kotlin/com/project/hexagonal/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/project/hexagonal/global/security/SecurityConfig.kt
@@ -1,5 +1,8 @@
 package com.project.hexagonal.global.security
 
+import com.project.hexagonal.global.filter.ExceptionHandlerFilter
+import com.project.hexagonal.global.filter.JwtRequestFilter
+import com.project.hexagonal.global.security.handler.CustomAccessDenideHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
@@ -9,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtRequestFilter: JwtRequestFilter,
+    private val exceptionHandlerFilter: ExceptionHandlerFilter
+) {
 
     @Bean
     protected fun filterChain(http: HttpSecurity): SecurityFilterChain =
@@ -28,12 +35,20 @@ class SecurityConfig {
 
             .authorizeRequests()
             .antMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
+            .antMatchers(HttpMethod.PATCH, "/api/v1/auth/**").permitAll()
+
             .antMatchers("/api/v1/admin/**").hasRole("ROLE_ADMIN")
+            .antMatchers("/api/v1/user/**").hasRole("ROLE_USER")
             .anyRequest().authenticated()
-
             .and()
-            .build()
 
+            .exceptionHandling()
+            .accessDeniedHandler(CustomAccessDenideHandler())
+            .and()
+
+            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .addFilterBefore(exceptionHandlerFilter, JwtRequestFilter::class.java)
+            .build()
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
