059ca73d7ff6ab6f72d48e5daaf693eeed9f526e
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-05 23:08:41 +0900

    :recycle: :: jwtParser로 네이밍 변경

--- src/main/kotlin/com/project/hexagonal/domain/account/application/usecase/ReissueTokenUseCase.kt
+++ src/main/kotlin/com/project/hexagonal/domain/account/application/usecase/ReissueTokenUseCase.kt
@@ -2,7 +2,7 @@ package com.project.hexagonal.domain.account.application.usecase
 
 import com.project.hexagonal.domain.account.adapter.web.data.response.SignInResponse
 import com.project.hexagonal.domain.account.application.port.GenetateJwtPort
-import com.project.hexagonal.domain.account.application.port.RefreshTokenParserPort
+import com.project.hexagonal.domain.account.application.port.JwtParserPort
 import com.project.hexagonal.domain.account.application.port.RefreshTokenPort
 import com.project.hexagonal.global.annotation.UseCase
 import com.project.hexagonal.global.security.jwt.exception.ExpiredRefreshTokenExcpetion
@@ -11,13 +11,13 @@ import com.project.hexagonal.global.security.jwt.exception.ExpiredRefreshTokenEx
 class ReissueTokenUseCase(
     private val genetateJwtPort: GenetateJwtPort,
     private val refreshTokenPort: RefreshTokenPort,
-    private val refreshTokenParserPort: RefreshTokenParserPort
+    private val jwtParserPort: JwtParserPort
 ) {
 
     fun execute(request: String): SignInResponse {
-        val refreshToken = refreshTokenParserPort.parseRefershToken(request)
+        val refreshToken = jwtParserPort.parseRefershToken(request)
         val refreshTokenDomain = refreshTokenPort.findByRefreshToken(refreshToken)
-        if (refreshTokenParserPort.isRefreshTokenExpired(refreshToken)) {
+        if (jwtParserPort.isRefreshTokenExpired(refreshToken)) {
             throw ExpiredRefreshTokenExcpetion()
         }
         return genetateJwtPort.generate(refreshTokenDomain.accountEmail)
