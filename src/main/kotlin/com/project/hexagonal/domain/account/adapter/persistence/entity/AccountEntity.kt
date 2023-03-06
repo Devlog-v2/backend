120ef0cdbae00815ba71e1c6854f62722b3e4dec
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-06 11:07:15 +0900

    :recycle: :: 테이블 이름 대문자 삭제

--- src/main/kotlin/com/project/hexagonal/domain/account/adapter/persistence/entity/AccountEntity.kt
+++ src/main/kotlin/com/project/hexagonal/domain/account/adapter/persistence/entity/AccountEntity.kt
@@ -1,7 +1,7 @@
 package com.project.hexagonal.domain.account.adapter.persistence.entity
 
 import com.project.hexagonal.domain.account.Account
-import com.project.hexagonal.domain.account.adapter.web.data.enumType.Autority
+import com.project.hexagonal.domain.account.adapter.presentation.data.enumType.Autority
 import com.project.hexagonal.global.entity.BaseIdxEntity
 import java.util.*
 import javax.persistence.*
@@ -13,7 +13,7 @@ class AccountEntity(
     val encodedPassword: String,
     val name: String,
     @ElementCollection(fetch = FetchType.EAGER) // 즉시 로딩
-    @Enumerated(EnumType.STRING) @CollectionTable(name = "account_Authority", joinColumns = [JoinColumn(name = "idx")])
+    @Enumerated(EnumType.STRING) @CollectionTable(name = "account_authority", joinColumns = [JoinColumn(name = "idx")])
     val authority: MutableList<Autority>
 ): BaseIdxEntity()
 
