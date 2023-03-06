9a73ada21c0b4c65651da7b149b8922b7ad4f3e4
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-06 12:30:05 +0900

    :recycle: :: readOnlyUseCase로 변경

--- src/main/kotlin/com/project/hexagonal/domain/account/application/usecase/ReissueTokenUseCase.kt
+++ src/main/kotlin/com/project/hexagonal/domain/account/application/usecase/ReissueTokenUseCase.kt
@@ -4,11 +4,11 @@ import com.project.hexagonal.domain.account.adapter.presentation.data.response.S
 import com.project.hexagonal.domain.account.application.port.GenetateJwtPort
 import com.project.hexagonal.domain.account.application.port.JwtParserPort
 import com.project.hexagonal.domain.account.application.port.RefreshTokenPort
-import com.project.hexagonal.global.annotation.UseCase
+import com.project.hexagonal.global.annotation.ReadOnlyUseCase
 import com.project.hexagonal.global.security.jwt.exception.ExpiredRefreshTokenExcpetion
 import com.project.hexagonal.global.security.jwt.exception.InvalidTokenTypeException
 
-@UseCase
+@ReadOnlyUseCase
 class ReissueTokenUseCase(
     private val genetateJwtPort: GenetateJwtPort,
     private val refreshTokenPort: RefreshTokenPort,
