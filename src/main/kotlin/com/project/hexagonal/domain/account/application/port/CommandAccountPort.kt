dcecf140ef0e4d5b22867d51cf485df1e4d74238
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-09 08:56:04 +0900

    :recycle: :: entity -> domain 반환으로 변경

--- src/main/kotlin/com/project/hexagonal/domain/account/application/port/CommandAccountPort.kt
+++ src/main/kotlin/com/project/hexagonal/domain/account/application/port/CommandAccountPort.kt
@@ -1,10 +1,9 @@
 package com.project.hexagonal.domain.account.application.port
 
 import com.project.hexagonal.domain.account.Account
-import com.project.hexagonal.domain.account.adapter.persistence.entity.AccountEntity
 
 interface CommandAccountPort {
 
-    fun saveAccount(account: Account, encodedPassword: String): AccountEntity
+    fun saveAccount(account: Account, encodedPassword: String): Account
 
 }
