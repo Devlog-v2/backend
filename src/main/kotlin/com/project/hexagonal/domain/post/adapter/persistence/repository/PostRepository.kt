7c81af91f5e67d7e3cf936fb6b9592a87075caf7
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-09 09:02:38 +0900

    :sparkles: :: post entity 추가

---
+++ src/main/kotlin/com/project/hexagonal/domain/post/adapter/persistence/repository/PostRepository.kt
@@ -0,0 +1,10 @@
+package com.project.hexagonal.domain.post.adapter.persistence.repository
+
+import com.project.hexagonal.domain.post.adapter.persistence.entity.PostEntity
+import org.springframework.data.repository.CrudRepository
+import java.util.UUID
+
+interface PostRepository: CrudRepository<PostEntity, UUID> {
+    fun findByIdx(idx: UUID): PostEntity?
+
+}
