5d08c954eb6fcb62074c87fe109dbcfd980b8d49
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-09 09:04:57 +0900

    :sparkles: :: comment entity 추가

---
+++ src/main/kotlin/com/project/hexagonal/domain/comment/adapter/persistence/CommentPersistenceAdapter.kt
@@ -0,0 +1,11 @@
+package com.project.hexagonal.domain.comment.adapter.persistence
+
+import com.project.hexagonal.global.annotation.AdapterWithTransaction
+
+@AdapterWithTransaction
+class CommentPersistenceAdapter(
+
+) {
+
+
+}
