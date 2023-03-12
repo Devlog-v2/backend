78c36bf1cf7798a03f9ea85437a7b4809a6e0d06
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-12 23:01:31 +0900

    :ambulance: :: cors config 추가

---
+++ src/main/kotlin/com/project/devlog/global/config/WebConfig.kt
@@ -0,0 +1,25 @@
+package com.project.devlog.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig: WebMvcConfigurer {
+    //CORS 설정
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:8080", "http://localhost:3000"
+            )
+            .allowedMethods(
+                HttpMethod.GET.name,
+                HttpMethod.HEAD.name,
+                HttpMethod.POST.name,
+                HttpMethod.PUT.name,
+                HttpMethod.DELETE.name,
+                HttpMethod.PATCH.name
+            )
+    }
+}
