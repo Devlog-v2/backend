a74995299095b77d17d399d3990ce79c5391fa2d
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-12 21:13:19 +0900

    :recycle: :: v2로 endPoint 변경

--- src/main/kotlin/com/project/devlog/domain/account/adapter/presentation/AccountAuthWebAdapter.kt
+++ src/main/kotlin/com/project/devlog/domain/account/adapter/presentation/AccountAuthWebAdapter.kt
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v2/auth")
 class AccountAuthWebAdapter(
     private val signUpUseCase: SignUpUseCase,
     private val signInUseCase: SignInUseCase,
--- src/main/kotlin/com/project/devlog/domain/like/adapter/presentation/LikeWebAdapter.kt
+++ src/main/kotlin/com/project/devlog/domain/like/adapter/presentation/LikeWebAdapter.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
-@RequestMapping("/like")
+@RequestMapping("api/v2/like")
 class LikeWebAdapter(
     private val saveLikeUseCase: SaveLikeUseCase,
     private val deleteLikeUseCase: DeleteLikeUseCase
