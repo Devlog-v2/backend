89e41eb3ea86c857583572c39d2e1776cb9bb0c8
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-14 14:32:21 +0900

    :recycle: :: uuid로 변경

--- src/main/kotlin/com/project/devlog/domain/comment/Comment.kt
+++ src/main/kotlin/com/project/devlog/domain/comment/Comment.kt
@@ -3,7 +3,7 @@ package com.project.devlog.domain.comment
 import java.util.UUID
 
 data class Comment(
-    val idx: Long,
+    val idx: UUID,
     val comment: String,
     val accountIdx: UUID,
     val postIdx: UUID
