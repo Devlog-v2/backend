29e31ec17deca9130f971b12791019c904a2bacc
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-14 14:08:17 +0900

    :sparkles: :: HealthCheck 컨트롤러 추가

---
+++ src/main/kotlin/com/project/devlog/global/health/HealthCheck.kt
@@ -0,0 +1,14 @@
+package com.project.devlog.global.health
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheck {
+
+    @GetMapping
+    fun health(): ResponseEntity<String> =
+        ResponseEntity.ok("GOOD")
+
+}
