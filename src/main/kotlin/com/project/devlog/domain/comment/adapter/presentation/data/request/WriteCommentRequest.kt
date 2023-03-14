49afeece7515548412f8ba871d0c59b56f32d773
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-14 14:34:00 +0900

    :sparkles: :: save useCase 추가

---
+++ src/main/kotlin/com/project/devlog/domain/comment/adapter/presentation/data/request/WriteCommentRequest.kt
@@ -0,0 +1,5 @@
+package com.project.devlog.domain.comment.adapter.presentation.data.request
+
+data class WriteCommentRequest(
+    val comment: String
+)
--- src/main/kotlin/com/project/devlog/domain/comment/application/usecase/SaveCommentUseCase.kt
+++ src/main/kotlin/com/project/devlog/domain/comment/application/usecase/SaveCommentUseCase.kt
@@ -1,7 +1,26 @@
 package com.project.devlog.domain.comment.application.usecase
 
+import com.project.devlog.domain.account.application.port.AccountSecurityPort
+import com.project.devlog.domain.comment.Comment
+import com.project.devlog.domain.comment.adapter.presentation.data.request.WriteCommentRequest
+import com.project.devlog.domain.comment.application.port.CommandCommentPort
 import com.project.devlog.global.annotation.UseCase
+import java.util.*
 
 @UseCase
-class SaveCommentUseCase {
+class SaveCommentUseCase(
+    private val commandCommentPort: CommandCommentPort,
+    private val accountSecurityPort: AccountSecurityPort
+) {
+
+    fun execute(postIdx: UUID, request: WriteCommentRequest): UUID {
+        val comment = Comment(
+            idx = UUID.randomUUID(),
+            comment = request.comment,
+            postIdx = postIdx,
+            accountIdx = accountSecurityPort.getCurrentAccountIdx(),
+        )
+        return commandCommentPort.saveComment(comment).idx
+    }
+
 }
