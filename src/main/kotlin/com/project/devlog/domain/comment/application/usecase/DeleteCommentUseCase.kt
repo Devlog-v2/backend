1ac6afea7e8b960c32c9495f311bad547b6e3932
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-14 14:35:00 +0900

    :sparkles: :: delete useCase 추가

--- src/main/kotlin/com/project/devlog/domain/comment/application/usecase/DeleteCommentUseCase.kt
+++ src/main/kotlin/com/project/devlog/domain/comment/application/usecase/DeleteCommentUseCase.kt
@@ -1,7 +1,20 @@
 package com.project.devlog.domain.comment.application.usecase
 
+import com.project.devlog.domain.comment.application.port.CommandCommentPort
+import com.project.devlog.domain.comment.application.port.QueryCommentPort
+import com.project.devlog.domain.comment.exception.CommentNotFountException
 import com.project.devlog.global.annotation.UseCase
+import java.util.UUID
 
 @UseCase
-class DeleteCommentUseCase {
+class DeleteCommentUseCase(
+    private val commandCommentPort: CommandCommentPort,
+    private val queryCommentPort: QueryCommentPort
+) {
+
+    fun execute(commentIdx: UUID) =
+        queryCommentPort.queryByCommentIdx(commentIdx)
+            .let { it ?: throw CommentNotFountException() }
+            .let { commandCommentPort.deleteComment(it) }
+
 }
