e939f4cb7cfb60be3fbfd0b2d8f0e669c125bde8
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-16 12:17:05 +0900

    :recycle: :: crudRepository로 변경

--- src/main/kotlin/com/project/devlog/domain/account/adapter/persistence/repository/AccountRepository.kt
+++ src/main/kotlin/com/project/devlog/domain/account/adapter/persistence/repository/AccountRepository.kt
@@ -2,10 +2,11 @@ package com.project.devlog.domain.account.adapter.persistence.repository
 
 import com.project.devlog.domain.account.adapter.persistence.entity.AccountEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 import java.util.UUID
 
 // 영속성이랑 도메인 계층을 구분하기 위해서 ORM Entity를 따로 만들었들어 구분 하였다
-interface AccountRepository: JpaRepository<AccountEntity, UUID>  {
+interface AccountRepository: CrudRepository<AccountEntity, UUID>  {
 
     fun existsByEmail(email: String): Boolean
     fun findByEmail(email: String): AccountEntity?
