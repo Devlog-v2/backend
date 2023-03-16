47ca6502af55c1cb229cd6174bd387a5d9eab339
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-16 20:14:28 +0900

    :recycle: :: UUID return 하게 변경

--- src/main/kotlin/com/project/devlog/domain/post/application/usecase/UpdatePostUseCase.kt
+++ src/main/kotlin/com/project/devlog/domain/post/application/usecase/UpdatePostUseCase.kt
@@ -5,7 +5,7 @@ import com.project.devlog.domain.post.application.port.CommandPostPort
 import com.project.devlog.domain.post.application.port.QueryPostPort
 import com.project.devlog.domain.post.exception.PostNotFoundException
 import com.project.devlog.global.annotation.UseCase
-import com.project.devlog.infrastructure.s3.application.port.S3Port
+import com.project.devlog.infrastructure.s3.application.port.S3UploadPort
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
 
@@ -13,18 +13,17 @@ import java.util.*
 class UpdatePostUseCase(
     private val commandPostPort: CommandPostPort,
     private val queryPostPort: QueryPostPort,
-    private val s3Port: S3Port
+    private val s3UploadPort: S3UploadPort
 ) {
 
-    fun execute(postIdx: UUID, fileList: MutableList<MultipartFile>?, request: UpdatePostRequest) {
+    fun execute(postIdx: UUID, fileList: MutableList<MultipartFile>?, request: UpdatePostRequest): UUID =
         queryPostPort.queryPostById(postIdx)
             .let { it ?: throw PostNotFoundException() }
             .let {
-                val uploadImages = fileList?.let { s3Port.uploadFile(fileList, "post/") }
+                val uploadImages = fileList?.let { s3UploadPort.uploadFile(fileList, "post/") }
                 commandPostPort.savePost(
                     it.copy(title = request.title, content = request.content, tag = request.tag, images = uploadImages)
                 )
-            }
-    }
+            }.idx
 
 }
