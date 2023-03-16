17e830d8f3d172b33ef2fc0b7523981ab8116fec
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-16 12:15:20 +0900

    :bug: :: 계속되는 권한 이슈로 account, admin details 변경

--- src/main/kotlin/com/project/devlog/domain/account/adapter/presentation/data/enumType/Authority.kt
+++ src/main/kotlin/com/project/devlog/domain/account/adapter/presentation/data/enumType/Authority.kt
@@ -1,11 +1,6 @@
 package com.project.devlog.domain.account.adapter.presentation.data.enumType
 
-import org.springframework.security.core.GrantedAuthority
-
-enum class Authority: GrantedAuthority {
-
-    ROLE_ACCOUNT, ROLE_ADMIN;
-
-    override fun getAuthority(): String = name
-
+enum class Authority {
+    ROLE_ACCOUNT,
+    ROLE_ADMIN
 }
