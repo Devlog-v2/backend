29a13ad4c52d8b6f33ebc793c146c7ca2cc90728
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-16 12:23:34 +0900

    :ambulance: :: 이메일 유효성 예외처리 추가

--- src/main/kotlin/com/project/devlog/domain/account/application/usecase/ReissueTokenUseCase.kt
+++ src/main/kotlin/com/project/devlog/domain/account/application/usecase/ReissueTokenUseCase.kt
@@ -3,7 +3,9 @@ package com.project.devlog.domain.account.application.usecase
 import com.project.devlog.domain.account.adapter.presentation.data.response.SignInResponse
 import com.project.devlog.domain.account.application.port.GenerateJwtPort
 import com.project.devlog.domain.account.application.port.JwtParserPort
+import com.project.devlog.domain.account.application.port.QueryAccountPort
 import com.project.devlog.domain.account.application.port.RefreshTokenPort
+import com.project.devlog.domain.account.exception.AccountNotFoundException
 import com.project.devlog.global.annotation.ReadOnlyUseCase
 import com.project.devlog.global.security.jwt.exception.ExpiredRefreshTokenExcpetion
 import com.project.devlog.global.security.jwt.exception.InvalidTokenTypeException
@@ -12,16 +14,19 @@ import com.project.devlog.global.security.jwt.exception.InvalidTokenTypeExceptio
 class ReissueTokenUseCase(
     private val generateJwtPort: GenerateJwtPort,
     private val refreshTokenPort: RefreshTokenPort,
-    private val jwtParserPort: JwtParserPort
+    private val jwtParserPort: JwtParserPort,
+    private val queryAccountPort: QueryAccountPort
 ) {
 
     fun execute(request: String): SignInResponse {
         val refreshToken = jwtParserPort.parseRefershToken(request) ?: throw InvalidTokenTypeException()
         val refreshTokenDomain = refreshTokenPort.queryByRefreshToken(refreshToken)
+        val account = queryAccountPort.queryAccountByIdx(refreshTokenDomain.accountIdx) ?: throw AccountNotFoundException()
+
         if (jwtParserPort.isRefreshTokenExpired(refreshToken)) {
             throw ExpiredRefreshTokenExcpetion()
         }
-        return generateJwtPort.generate(refreshTokenDomain.accountIdx)
+        return generateJwtPort.generate(refreshTokenDomain.accountIdx, account.authority)
     }
 
 }
