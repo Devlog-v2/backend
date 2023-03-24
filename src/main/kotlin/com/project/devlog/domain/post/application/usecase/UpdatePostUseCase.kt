84b37c3b2c8f244bc36464a7d9f2087e030e2906
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-03-24 22:00:44 +0900

    :recycle: :: 이미지 생성 로직 변경

--- src/main/kotlin/com/project/devlog/domain/post/application/usecase/UpdatePostUseCase.kt
+++ src/main/kotlin/com/project/devlog/domain/post/application/usecase/UpdatePostUseCase.kt
@@ -5,24 +5,20 @@ import com.project.devlog.domain.post.application.port.CommandPostPort
 import com.project.devlog.domain.post.application.port.QueryPostPort
 import com.project.devlog.domain.post.exception.PostNotFoundException
 import com.project.devlog.global.annotation.UseCase
-import com.project.devlog.infrastructure.s3.application.port.S3UploadPort
-import org.springframework.web.multipart.MultipartFile
 import java.util.*
 
 @UseCase
 class UpdatePostUseCase(
     private val commandPostPort: CommandPostPort,
     private val queryPostPort: QueryPostPort,
-    private val s3UploadPort: S3UploadPort
 ) {
 
-    fun execute(postIdx: UUID, fileList: MutableList<MultipartFile>?, request: UpdatePostRequest): UUID =
+    fun execute(postIdx: UUID, request: UpdatePostRequest): UUID =
         queryPostPort.queryPostById(postIdx)
             .let { it ?: throw PostNotFoundException() }
             .let {
-                val uploadImages = fileList?.let { s3UploadPort.uploadFile(fileList, "post/") }
                 commandPostPort.savePost(
-                    it.copy(title = request.title, content = request.content, tag = request.tag, images = uploadImages)
+                    it.copy(title = request.title, content = request.content, tag = request.tag)
                 )
             }.idx
 
